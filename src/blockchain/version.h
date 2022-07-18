f10ea4a1279508774dc943b44fa0b5bdb679a726
qmhntreew <qmhntreew@hotmail.com>
2022-07-18 10:46:39 +0800

    Modify version to v1.0.0

--- src/blockchain/version.h
+++ src/blockchain/version.h
@@ -12,9 +12,9 @@
 
 #define VERSION_NAME "MetabaseNet"
 
-#define VERSION_MAJOR 0
+#define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_REVISION 1
+#define VERSION_REVISION 0
 
 std::string FormatVersion(int nVersion);
 
