bb7686179a73a2ebf30dc09e97077299db5e1c39
lonicera <lonicera.xyz@gmail.com>
2024-05-28 18:52:12 +0800

    Change default data dir to metabasenet.

--- debug.sh
+++ debug.sh
@@ -2,4 +2,4 @@
 
 GMNT=$HOME/Workspace/metabasenet/build/bin/gmnt
 
-dlv exec $GMNT -- --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.ethereum/testnet/password.txt
+dlv exec $GMNT -- --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.metabasenet/testnet/password.txt
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gmnt --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.ethereum/testnet/password.txt
+gmnt --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.metabasenet/testnet/password.txt
