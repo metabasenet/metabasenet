aa8c8022ca24302a687737962b022232838db059
lonicera <lonicera.xyz@gmail.com>
2024-05-29 19:59:19 +0800

    Change script files.

--- debug.sh
+++ debug.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-GMNT=$HOME/Workspace/metabasenet/build/bin/gmnt
+GMNT=$HOME/Workspace/github/metabasenet/build/bin/gmnt
+DATADIR=$HOME/Library/Metabasenet
 
-dlv exec $GMNT -- --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.metabasenet/testnet/password.txt
+dlv exec $GMNT -- --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $DATADIR/testnet/password.txt
--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-gmnt --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $HOME/.metabasenet/testnet/password.txt
+DATADIR=$HOME/Library/Metabasenet
+
+gmnt --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $DATADIR/testnet/password.txt
