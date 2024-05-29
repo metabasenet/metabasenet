cde9a497524d5044ace24bc70d6b76301265ec15
lonicera <lonicera.xyz@gmail.com>
2024-05-29 20:00:38 +0800

    Delete debug files.

--- debug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-GMNT=$HOME/Workspace/github/metabasenet/build/bin/gmnt
-DATADIR=$HOME/Library/Metabasenet
-
-dlv exec $GMNT -- --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $DATADIR/testnet/password.txt
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DATADIR=$HOME/Library/Metabasenet
-
-gmnt --testnet --mine --miner.etherbase 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --unlock 0x5962974eeb0b17b43EDaBFc9b747839317AA852F --password $DATADIR/testnet/password.txt
